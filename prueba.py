647125edf028e5c50fa0d9dfe074b4b3edda0bbc
cabustillo13 <cabustillo13@hotmail.com>
2020-12-14 17:49:30 -0300

    [Fix] Gripper Movement

--- prueba.py
+++ prueba.py
@@ -1,10 +1,29 @@
+#!/usr/bin/env python3
 import serial
+import socket
 import time
 
-#Puerto
-puerto = "/dev/ttyUSB0"
+# Definimos los datos iniciales
+HOST0 = '192.168.1.130'       # The server's hostname or IP address
+PORT0 = 65430                # The port used by the server
 
-ser = serial.Serial(puerto,9600)
-time.sleep(2)
-ser.write(b'300')
-ser.close()
+#Puerto Serial Arduino
+puerto = "/dev/ttyUSB1"
+arduino = serial.Serial(puerto, 9600)   # create serial object named arduino
+
+while(True):
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s0:
+        s0.connect((HOST0, PORT0))
+        
+        #Dato recibido
+        time.sleep(1)
+        data0 = float(s0.recv(1024))
+        print(data0)
+        #data0 = str(input("angulo: "))
+        
+        arduino.write(bytes(str(data0),'utf-8'))
+        arduino.flush()
+        #reachedPos = str(arduino.readline())
+        #print(reachedPos)
+        
